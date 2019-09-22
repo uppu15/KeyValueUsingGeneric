5c3f05d600c1100f14a75130645fe6a74cf7a40a
Jehyun Park <uppu1507@gmail.com>
2019-09-21 23:14:40 -0700

    Add project files.

---
+++ KeyValueUsingGenericType/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyValueUsingGenericType
+{
+    public struct KeyValue<T>
+    {
+        public KeyValue(string key, T value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+        public readonly string Key;
+        public readonly T Value;
+    }
+
+    public class MyDictionary<T>
+    {
+        int count = 0;
+
+        KeyValue<T>[] keyValueArray = new KeyValue<T>[10];
+
+        public T this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < keyValueArray.Length; i++)
+                {
+                    if (keyValueArray[i].Key == index)
+                        return keyValueArray[i].Value;
+                }
+                throw new KeyNotFoundException(index);
+            }
+            set
+            {
+                for (int i = 0; i < keyValueArray.Length; i++)
+                {
+                    if (keyValueArray[i].Key == index)
+                    {
+                        keyValueArray[i] = new KeyValue<T>(index, value);
+                        return;
+                    }
+                }
+                for (int i = 0; i < keyValueArray.Length; i++)
+                {
+                    if (keyValueArray[i].Key == null)
+                    {
+                        keyValueArray[i] = new KeyValue<T>(index, value);
+                        count++;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ KeyValueUsingGenericType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeyValueUsingGenericType
+{
+    class Program
+    {
+        static void Main()
+        {
+            var d = new MyDictionary<object>();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+}
